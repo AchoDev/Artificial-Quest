63f90a55f3b5fb41e1b678a3e1f70722572edccb
AchoDev <ahmedhdasi89@gmail.com>
2024-09-17 17:07:06 +0200

    removed app .listen

--- backend/server.ts
+++ backend/server.ts
@@ -15,11 +15,6 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on port 3000")
-})
-
-
 enum GameStatus {
     Lobby,
     ChooseItems,
